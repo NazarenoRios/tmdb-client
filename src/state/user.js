9c5450a18c630a0b358f195ea6f0b248f5b15270
NazarenoRios <nazarenolrios@gmail.com>
2022-10-01 12:13:17 -0300

    noinstance

--- src/state/user.js
+++ src/state/user.js
@@ -1,14 +1,14 @@
 import { createAsyncThunk, createReducer } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const instance = axios.create({
-    withCredentials: true,
-    credentials:'include',
-    baseURL: 'https://butterflix.onrender.com'
- })
+// const instance = axios.create({
+//     withCredentials: true,
+//     credentials:'include',
+//     baseURL: 'https://butterflix.onrender.com'
+//  })
 
 export const sendRegisterRequest = createAsyncThunk("register", ({email, password, name, lastname}) => {
-    return instance.post("/api/users/register", {
+    return axios.post("https://butterflix.onrender.com/api/users/register", {
         email: email.value,
         password: password.value,
         name: name.value,
@@ -18,28 +18,28 @@ export const sendRegisterRequest = createAsyncThunk("register", ({email, passwor
 })
 
 export const sendLoginRequest = createAsyncThunk("login", ({email, password}) => {
-    return instance.post("/api/users/login", {
+    return axios.post("https://butterflix.onrender.com/api/users/login", {
         email: email.value,
         password: password.value,
       }, { withCredentials: true, credentials: 'include' })
       .then(() => {
-        return instance.get("/api/users/me", { withCredentials: true, credentials: 'include' })
+        return axios.get("https://butterflix.onrender.com/api/users/me", { withCredentials: true, credentials: 'include' })
             .then(res => res.data)
       })
 })
 
 export const checkLogin = createAsyncThunk("check", () => {
-    return instance.get("/api/users/me", { withCredentials: true, credentials: 'include' })
+    return axios.get("https://butterflix.onrender.com/api/users/me", { withCredentials: true, credentials: 'include' })
       .then(res => res.data)
 })
 
 export const logOut = createAsyncThunk("LOG_OUT", () => {
-    return instance.post("/api/users/logout", { withCredentials: true, credentials: 'include' })
+    return axios.post("https://butterflix.onrender.com/api/users/logout", { withCredentials: true, credentials: 'include' })
       .then(res => res.data)
 })
 
 export const updateProfile = createAsyncThunk("UPDATE_PROFILE", ({name,lastname}) => {
-    return instance.put("/api/users/profile", {
+    return axios.put("https://butterflix.onrender.com/api/users/profile", {
         name: name.value,
         lastname: lastname.value,
     }, { withCredentials: true, credentials: 'include' })
@@ -47,35 +47,35 @@ export const updateProfile = createAsyncThunk("UPDATE_PROFILE", ({name,lastname}
 })
 
 export const updateProfileName = createAsyncThunk("UPDATE_PROFILE", (lastname) => {
-    return instance.put("/api/users/profile", {
+    return axios.put("https://butterflix.onrender.com/api/users/profile", {
         lastname: lastname.value,
     }, { withCredentials: true, credentials: 'include' })
       .then(res => res.data)
 })
 
 export const updateProfileLastname = createAsyncThunk("UPDATE_PROFILE", (name) => {
-    return instance.put("/api/users/profile", {
+    return axios.put("https://butterflix.onrender.com/api/users/profile", {
         name: name.value,
     }, { withCredentials: true, credentials: 'include' })
       .then(res => res.data)
 })
 
 export const updateProfilePicture = createAsyncThunk("UPDATE_PROFILE_PICTURE", (pic) => {
-    return instance.put("/api/users/profile", {
+    return axios.put("https://butterflix.onrender.com/api/users/profile", {
         pic: pic
     }, { withCredentials: true, credentials: 'include' })
       .then(res => res.data)
 })
 
 export const changePassword = createAsyncThunk("CHANGE_PASSWORD", ({password}) => {
-    return instance.put("/api/users/changePassword", {
+    return axios.put("https://butterflix.onrender.com/api/users/changePassword", {
         password: password.value,
     }, { withCredentials: true, credentials: 'include' })
       .then(res => res.data)
 })
 
 export const getAllUsers = createAsyncThunk("GET_USERS", (setUsers) => {
-    return instance.get("/api/users/")
+    return axios.get("https://butterflix.onrender.com/api/users/")
       .then(res => setUsers(res.data))
 }, { withCredentials: true, credentials: 'include' })
 
